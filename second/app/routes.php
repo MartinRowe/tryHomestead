8f1f433bb81ef399b121e8ba5bcb3c3859625723
Martin Rowe <martin.rowe@assessandimprove.co.uk>
2014-07-11 23:51:25 +0100

    Added Rowe

---
+++ second/app/routes.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function()
+{
+	return View::make('hello');
+});
+Route::get('trysass', function()
+{
+	return View::make('trysass');
+});
+Route::get('users',function()
+{
+ return View::make('users');
+});
+Route::get('martin', function()
+{
+    return 'Martin Rowe!';
+});
+Route::get('users', function()
+{
+	$users = DB::table('Users')->get();
+	return $users;
+//	return View::make('users.index', ['users' => $users]);
+});
---
+++ second/app/views/trysass.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0">
+    
+    <title>Assess and Improve</title>
+   <link rel="stylesheet" href="../public/css/ui/styles.css">  
+   
+</head>
+<body>
+<p>Trying Sass</p>
+</body>
+</html>
